f1bd73f407e9c6feefb0b934d52532c8d641c2d5
Amulya-T <71450912+Amulya-T@users.noreply.github.com>
2025-05-07 19:01:06 -0700

    Delete backend directory

--- backend/models/queryModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const QuerySchema = new mongoose.Schema({
-  query: { type: String, required: true },
-  response: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Query", QuerySchema);
--- backend/routes/searchRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { processQuery } = require("../controllers/searchController");
-const router = express.Router();
-
-router.post("/", processQuery);
-
-module.exports = router;
